db6bfa6a58603ab4e3efad91de735d79cdfb582e
sasha-fedyura <35074696+sasha-fedyura@users.noreply.github.com>
2021-03-29 23:10:55 +0300

    Add files via upload

---
+++ Lab (8-9)/mightygumball.js
@@ -0,0 +1,38 @@
+window.onload = init;
+
+function init() {
+	var url = "http://gumball/sales.json";
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			updateSales(request.responseText);
+		}
+	};
+	request.send(null);
+}
+
+function getSales() {
+	var url = "http://localhost/gumball/sales.json";
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.onload = function() {
+		if (request.status == 200) {
+			updateSales(request.responseText);
+		}
+	};
+	request.send(null);
+}
+
+function updateSales(responseText) {
+	var salesDiv = document.getElementById("sales");
+	var sales = JSON.parse(responseText);
+	for (var i = 0; i < sales.length; i++) {
+		var sale = sales[i];
+		var div = document.createElement("div");
+		div.setAttribute("class", "saleItem");
+		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
+		salesDiv.appendChild(div);
+	}
+}
+
